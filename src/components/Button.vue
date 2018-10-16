1c6e5a1b0f1f22dfe2cf7d36269810a5ac05078e
Victor Henrique Santiago <victorhsantiago3@gmail.com>
2018-10-15 22:24:04 -0300

    second commit

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="button" :class="{double,triple,operation}">
+    <button class="button" :class="{double, triple, operation}" @click="$emit('onClick', label)">
         {{label}}
     </button>
 </template>
